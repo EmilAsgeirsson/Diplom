c27f959c04ce8e38df1538f1a71dbc323e7d101d
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2020-09-23 21:19:58 +0200

    balls n bins

--- DataStructuresAndAlgorithms/BallsNBins/BallsNBins.cpp
+++ DataStructuresAndAlgorithms/BallsNBins/BallsNBins.cpp
@@ -4,18 +4,43 @@
 #include <time.h>
 
 
-int main()
+std::vector<std::vector<int>> fillRandomly(std::vector<std::vector<int>> bins)
 {
     // Place ball randomly in bins.
-    std::vector<std::vector<int>> bins(32749);
-    srand(time(NULL));
     int ball = 3;
     for (int i = 0; i < bins.size();i++) {
         int randIndex = rand() % bins.size();
         bins[randIndex].push_back(ball);
     }
+    return bins;
+}
 
-    // Search the bins
+
+std::vector<std::vector<int>> twoChoices(std::vector<std::vector<int>> bins)
+{
+    // Pick two bins randomly and fill up the lowest of the two.
+    int ball = 3;
+    for (int i = 0; i < bins.size();i++) {
+        int randIndex1 = rand() % bins.size();
+        int randIndex2 = rand() % bins.size();
+        if (bins[randIndex1].size() > bins[randIndex2].size())
+        {
+            bins[randIndex2].push_back(ball);
+        }
+        else
+        {
+            bins[randIndex1].push_back(ball);
+        }
+        
+    }
+    return bins;
+}
+
+
+void print(std::string title, std::vector<std::vector<int>> bins)
+{
+    std::cout << title << std::endl;
+
     int count = 0;
     int max = 0;
 
@@ -35,11 +60,30 @@ int main()
             numberOfBigBins++;
     }
 
+    std::cout << "max = " << max << " and bin number = " << count << " and the number of big bins is " << numberOfBigBins << std::endl;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    std::vector<std::vector<int>> bins(32749);    
+
+    std::vector<std::vector<int>> binsRandom = fillRandomly(bins);
+    std::vector<std::vector<int>> binsTwoChoices = twoChoices(bins);
+    
+    print("Randomly filled", binsRandom);
+    print("Two Choices", binsTwoChoices);
+
     // With 10007 balls. The maximum bin size is usually seven. I've seen 8 once.
     // With 32749 balls. The maximum bin size is usually seven. I've seen 8 once.
 
+    std::cout << "Ordinay:\t\t" << log(32749) / log(log(32749)) << std::endl;
+    std::cout << "Two Choices:\t" << log(log(32749)) << std::endl;
+    // No this is wrong.
+    
 
-    std::cout << "max = " << max << " and bin number = " << count << " and the number of big bins is " << numberOfBigBins << std::endl;
+    
     
     /*int yada = 0;
     std::cout << bins[count].size() << std::endl;
